ae4cfcac1a51c17248be1d47b8a848fad7d2b0b9
itsmeFer <ferdinandsianturi28@gmail.com>
2024-06-02 22:40:47 +0700

    php course

---
+++ course_ci_ferdi/application/controllers/Welcome.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('welcome_message');
+	}
+	public function coba(){
+		echo "Ini adalah controller Welcome, Method coba";
+	}
+}
